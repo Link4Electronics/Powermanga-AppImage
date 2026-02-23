f38337792cf437a0816082a8263f53dae6171ec5
Link4Electronics <link4electronics@gmail.com>
2026-02-22 22:26:14 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,7 +15,7 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/powermanga
-echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
+#echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 
